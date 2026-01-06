0838b775a43f09729961addc805ca9ef210f3b40
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-06 22:14:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+from config import BOT_TOKEN
+from bot.start import start
+from bot.downloader import download_handler
+
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, download_handler)
+    )
+
+    print("🤖 Insta & YouTube Helper Bot running...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
