8c37c20159d4a3b78bdc3177944eb54cd916683a
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-07 22:43:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 def home():
     return "Bot is running"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
