4a5ea356eadc269b1bc3068f80999bfd57ca8309
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-07 18:36:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,17 @@ from bot.start import start
 from bot.downloader import download_handler
 
 
-def main():
-    app = (
-        ApplicationBuilder()
-        .token(BOT_TOKEN)
-        .build()
-    )
+def run_bot():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, download_handler)
     )
 
-    print("🤖 Insta & YouTube Helper Bot running (Background Worker)")
+    print("🤖 Insta & YouTube Helper Bot running...")
     app.run_polling(drop_pending_updates=True)
 
 
 if __name__ == "__main__":
-    main()
+    run_bot()
