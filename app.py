daac3f3482c1a9542c047e2f99681ad5cf9648bf
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-07 21:25:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🤖 Telegram Bot is running!"
+    return "Bot is running"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
