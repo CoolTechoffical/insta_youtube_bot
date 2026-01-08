92f65b66e93794ce2dc2019a965202c1ba7289a6
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-08 23:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ def main():
 
     def __init__(self):
         super().__init__(
-            name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
