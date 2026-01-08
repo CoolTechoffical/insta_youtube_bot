32063c833d02ce87e6f6fca10acb3099f2c7be52
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-08 18:35:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_USERNAME = "media_boot_bot"
-DOWNLOAD_DIR = "/tmp"
-MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2GB
+DOWNLOAD_DIR = "/tmp/videos"  # SAFE on Render
+MAX_CONVERT_DURATION = 1800   # 30 minutes
+MAX_CONVERT_SIZE_MB = 700 
