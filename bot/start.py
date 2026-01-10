5146c2138c511946d5ac3678b3b8a8e89ac62d38
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-10 22:24:48 +0530

    Delete bot directory

--- bot/start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "👋 Welcome!\n\n"
-        "🤖 I am your Instagram & YouTube Helper Bot.\n"
-        "📥 Send me a link to get started."
-    )
