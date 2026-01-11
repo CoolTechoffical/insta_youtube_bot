ea5f24a4226c87ecf4c240b3055963e67d003cd0
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-11 14:13:04 +0530

    Update user_settings.py

--- user_settings.py
+++ user_settings.py
@@ -4,4 +4,4 @@ def set_count(user_id, count):
     user_settings[user_id] = count
 
 def get_count(user_id):
-    return user_settings.get(user_id, 10)  # default 10
+    return user_settings.get(user_id, 100)  # default 100
