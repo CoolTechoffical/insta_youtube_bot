2e95d12e08751cfab0fbce9813c71d1c679ab975
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-11 22:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import asyncio
+import threading
+from flask import Flask
+from bot import bot
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Bot is alive"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask, daemon=True).start()
+
+    # Run pyrogram in MAIN thread
+    bot.run()
