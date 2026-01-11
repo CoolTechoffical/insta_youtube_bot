d4fa6ca599edab9bae7f6ee1a781d0e682eae316
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-11 20:54:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
 # 🔹 Bot token (from @BotFather)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
 
-MONGO_URI = os.getenv("MONGO_URI")
 
 MAX_IMAGES = 200
 MAX_SAFE_SECONDS = 18   # Render safe
-MAX_WIDTH = 1280
