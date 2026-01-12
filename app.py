26165854a13e986dc87b16694c3585350dcc1b11
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-12 18:33:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+import asyncio
 import threading
 from flask import Flask
-from bot import bot, resume_tasks
+from bot import bot
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Highlight Bot Running"
+    return "✅ Bot is alive"
 
 def run_flask():
     app.run(host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
     threading.Thread(target=run_flask, daemon=True).start()
-    bot.run(resume_tasks())
+
+    # Run pyrogram in MAIN thread
+    bot.run()
