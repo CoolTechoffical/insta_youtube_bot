6f9890da0412ea46af68a56380e5a8ae080fe9f3
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-12 18:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-import asyncio
 import threading
-from flask import Flask
-from bot import bot
+from flask import Flask, jsonify
+from bot import bot, tasks
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Bot is alive"
+    return "✅ Highlight Bot Running"
+
+@app.route("/status")
+def status():
+    return jsonify(tasks)
 
 def run_flask():
     app.run(host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
+    # Start webserver in background
     threading.Thread(target=run_flask, daemon=True).start()
-
-    # Run pyrogram in MAIN thread
-    bot.run()
+    # Resume pending tasks
+    bot.run(resume_pending_tasks())
