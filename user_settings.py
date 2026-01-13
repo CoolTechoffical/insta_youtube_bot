4fa699ad221ee068278608c9b201e06c6ec83368
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-13 23:54:58 +0530

    Update user_settings.py

--- user_settings.py
+++ user_settings.py
@@ -3,7 +3,7 @@ user_settings = {}
 def get_user(user_id):
     if user_id not in user_settings:
         user_settings[user_id] = {
-            "count": 100,
+            "count": 25,
             "nsfw": True
         }
     return user_settings[user_id]
