789c873d1ede93b8ff7b4beeafee9402da6fbb74
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-13 23:54:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
 
 # 🔹 Bot token (from @BotFather)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+DOWNLOAD_DIR = "downloads"
+OUTPUT_DIR = "output"
+
+MAX_FRAMES_LIMIT = 200  # hard server safety limit
+DEFAULT_EXTRACT_COUNT = 25
