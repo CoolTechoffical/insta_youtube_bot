0158b61c006547569beac10f3fe503baa801dfa4
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-01-22 23:02:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import asyncio
-import threading
 from flask import Flask
-from bot import bot
+from bot import BOT
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Bot is alive"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=10000)
+    return "✅ AI Detector Bot is running!"
 
 if __name__ == "__main__":
-    threading.Thread(target=run_flask, daemon=True).start()
-
-    # Run pyrogram in MAIN thread
-    bot.run()
+    loop = asyncio.get_event_loop()
+    loop.create_task(BOT.start())
+    app.run(host="0.0.0.0", port=8080)
