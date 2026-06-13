40e492a31ea474af1b6177120f18256659ef5987
COOL_TECH <156781377+CoolTechoffical@users.noreply.github.com>
2026-06-13 19:09:09 +0530

    Update down.py

--- down.py
+++ down.py
@@ -7,18 +7,41 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 def download_video_from_url(url):
 
-    ydl_opts = {
-        "outtmpl": f"{DOWNLOAD_DIR}/url_%(id)s.%(ext)s",
-        "quiet": True,
-        "noplaylist": True,
-        "merge_output_format": "mp4"
-    }
+ydl_opts = {
+    "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
+    "quiet": True,
+    "noplaylist": True,
+    "merge_output_format": "mp4",
+    "restrictfilenames": True
+}
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
-        info = ydl.extract_info(
-            url,
-            download=True
-        )
+    info = ydl.extract_info(
+        url,
+        download=True
+    )
 
-        return ydl.prepare_filename(info)
+    video_path = ydl.prepare_filename(info)
+
+    if os.path.exists(video_path):
+        return video_path
+
+    base = os.path.splitext(video_path)[0]
+
+    for ext in (
+        ".mp4",
+        ".mkv",
+        ".webm",
+        ".mov",
+        ".avi"
+    ):
+
+        candidate = base + ext
+
+        if os.path.exists(candidate):
+            return candidate
+
+    raise FileNotFoundError(
+        "Downloaded file not found"
+    )
